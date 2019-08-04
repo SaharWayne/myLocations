26d739aaa834ad2cf2aa7ec7e7df86bfc866c039
SaharWayne <sahar014g@gmail.com>
2019-08-04 05:38:31 +0300

    Updates

---
+++ precache-manifest.5728b973cfeea6644dbbf61fcf8ceee4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373b3d51a21dadf7e97501a06ec6d566",
+    "url": "/myLocations/index.html"
+  },
+  {
+    "revision": "c07f83341768b128bd13",
+    "url": "/myLocations/static/css/2.e866221e.chunk.css"
+  },
+  {
+    "revision": "d3ff6d07b74192f40599",
+    "url": "/myLocations/static/css/main.ff444e40.chunk.css"
+  },
+  {
+    "revision": "c07f83341768b128bd13",
+    "url": "/myLocations/static/js/2.aa25b588.chunk.js"
+  },
+  {
+    "revision": "d3ff6d07b74192f40599",
+    "url": "/myLocations/static/js/main.aa2f6db8.chunk.js"
+  },
+  {
+    "revision": "89ea5d039bd13695091d",
+    "url": "/myLocations/static/js/runtime~main.e7a79ff9.js"
+  },
+  {
+    "revision": "b969e9fc8f5103dbe4a37a4b8266fb0e",
+    "url": "/myLocations/static/media/add.b969e9fc.svg"
+  },
+  {
+    "revision": "cefc206c2f177cd0794baa8cca85f3d8",
+    "url": "/myLocations/static/media/arrow.cefc206c.svg"
+  },
+  {
+    "revision": "a8fb9acf5abd9acf8d19919b5b8d7c35",
+    "url": "/myLocations/static/media/categories.a8fb9acf.svg"
+  },
+  {
+    "revision": "9250991555f3934e0e9924650a87a622",
+    "url": "/myLocations/static/media/category.92509915.svg"
+  },
+  {
+    "revision": "add831dda68b5d36b03ace63a5aba251",
+    "url": "/myLocations/static/media/copy.add831dd.svg"
+  },
+  {
+    "revision": "a58d3d1145c84c03ad547c1dad1cdaeb",
+    "url": "/myLocations/static/media/edit.a58d3d11.svg"
+  },
+  {
+    "revision": "ed9263f12609e5ba63f37f14ca51a4fc",
+    "url": "/myLocations/static/media/item-bg.ed9263f1.jpg"
+  },
+  {
+    "revision": "2077207ce70de107d5b82b6a4e756713",
+    "url": "/myLocations/static/media/locations.2077207c.svg"
+  },
+  {
+    "revision": "6690bdd0c6f447310531e693e2b3aa4e",
+    "url": "/myLocations/static/media/remove.6690bdd0.svg"
+  }
+]);
